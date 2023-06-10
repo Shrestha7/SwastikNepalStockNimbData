cccf308ece91aace9fd8d636082655a8c2c8ce1d
swastik shrestha <40354116+Shrestha7@users.noreply.github.com>
2023-06-10 22:56:39 +0545

    Update SwastikNepalStockNimbDataDump.py
    <> is working and inserting in database

--- SwastikNepalStockNimbDataDump.py
+++ SwastikNepalStockNimbDataDump.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 from decouple import config
 from fake_useragent import UserAgent
@@ -49,7 +50,7 @@ soup = BeautifulSoup(html, "html.parser")
 #Find all the td tags inside table tags and extract the text content
 # data = soup.find_all("td")
 data = soup.select("table.table tr td")[0:13]
-# print(data[::])
+print(data[::])
 # Connect to Mysql
 connection = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=DATABASE)
 
@@ -87,7 +88,7 @@ try:
                 AsOfDate DATETIME DEFAULT CURRENT_TIMESTAMP,
                 InstrumentType TEXT,
                 ListingDate TEXT ,
-                LastTradedPrice TEXT,
+                LastTradedPrice CHAR,
                 TotalTradedQuantity TEXT ,
                 TotalTrades TEXT ,
                 PreviousDayClosePrice TEXT ,
@@ -109,17 +110,75 @@ try:
         # check if <td> tag if condtion
         # if td tag then gets text and remove whitespaces due to strip = true.
         
+        # row = [
+        #     item.get_text(strip=True).replace(",", "")
+        #     for item in data
+        #     if item.name == "td"
+        # ]
+        
         row = [
             item.get_text(strip=True).replace(",", "")
-            for item in data
             if item.name == "td"
+            else str(item)
+            for item in data
         ]
-        # print(row)
+        
+        print(row)
         # check row list= 14, if !=14 raises valueerror.
         # to ensure the data is scraped correctly,
         # to prevent incorrect data from being inserted into database.
         if len(row) != 13:
             raise ValueError("Expected 13 items in row, got %d" % len(row))
+        
+        # # Extract the components of LastTradedPrice
+        # last_traded_price_element = data[2].find("div", class_="nochange")
+        # price = last_traded_price_element.find("span").get_text(strip=True)
+        # change_elements = last_traded_price_element.find_all("i")
+        # changes = []
+        # for element in change_elements:
+        #     class_attr = element.get("class")
+        #     if class_attr:
+        #         icon_names = [name[3:] for name in class_attr if name.startswith("fa-")]
+        #         changes.extend(icon_names)
+        # unicode_changes = []
+        # for change in changes:
+        #     try:
+        #         unicode_changes.append(chr(int(change.replace('-', ''), 16)))
+        #     except ValueError:
+        #         # Handle unexpected values, such as 'angleleft'
+        #         pass
+        # percentage = last_traded_price_element.find_all("span")[-1].get_text(strip=True)
+
+        # # Build the LastTradedPrice value for the row
+        # last_traded_price = f"{price} {' '.join(unicode_changes)} {percentage}"
+        
+        last_traded_price_element = data[2].find("div", class_="nochange")
+        price = last_traded_price_element.find("span").get_text(strip=True)
+        change_elements = last_traded_price_element.find_all("i")
+        changes = []
+        for element in change_elements:
+            class_attr = element.get("class")
+            if class_attr:
+                icon_names = [name.replace("fa-", "") for name in class_attr if name.startswith("fa-")]
+                changes.extend(icon_names)
+        percentage = last_traded_price_element.find_all("span")[-1].get_text(strip=True)
+
+        # Build the LastTradedPrice value for the row
+        change_values = [change for change in changes if change != "angle-left" and change != "angle-right"]
+        change_value = " ".join(change_values) if change_values else "0.00"  # Use change_values if available, otherwise set to "0.00"
+        if change_value != "0.00" and change_value.startswith("-"):
+            last_traded_price = f"{price} {change_value} {percentage}"
+        else:
+            last_traded_price = f"{price} <> {change_value} {percentage}"
+        
+        # Replace font-awesome class names with unicode values
+        unicode_mapping = {
+            'fa-angle-left': '\u2190',
+            'fa-angle-right': '\u2192',
+            # Add more mappings for other font-awesome class names
+        }
+        unicode_changes = [unicode_mapping.get(change, change) for change in changes]
+
         # Insert the data
         # sql = """
         #     INSERT INTO nepalstock(
@@ -169,7 +228,8 @@ try:
         # into the corresponding columns of the table
         # in the database.
         # tuple function used to convert row list into tuple.
-        cursor.execute(sql, tuple(row))
+        #cursor.execute(sql, tuple(row))
+        cursor.execute(sql, tuple(row[:2] + [last_traded_price] + row[3:]))
         # Commit the changes
         connection.commit()
 
@@ -178,7 +238,7 @@ finally:
     # close the connection
     connection.close()
 
-os.system("taskkill /f /im geckodriver.exe")
+#os.system("taskkill /f /im geckodriver.exe")
 # write the data to a Notepad file
 # with open("data.txt", "w", encoding='utf-8') as file:
 #     for item in data:
